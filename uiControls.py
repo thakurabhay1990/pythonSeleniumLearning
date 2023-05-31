28d9b4030203b4deb410a7db9fe628d8ac6abcfa
Abhay Thakur <thakurabhay1990@gmail.com>
2023-05-31 17:18:07 +0530

    validated displayed text box, hide it and again validated via assertion

--- uiControls.py
+++ uiControls.py
@@ -35,4 +35,9 @@ radiobuttons = driver.find_elements(By.CSS_SELECTOR, ".radioButton")
 radiobuttons[2].click()
 assert radiobuttons[2].is_selected()
 
+# Validate the display text box and then hide it and again validate that.
+assert driver.find_element(By.ID, "displayed-text").is_displayed()
+driver.find_element(By.ID, "hide-textbox").click()
+assert not driver.find_element(By.ID, "displayed-text").is_displayed()
+
 driver.close()
